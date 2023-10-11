ead23bd2501a33e6dafb9d254630b10f8056413a
ddakae23 <ddakae23@gmail.com>
2023-10-11 11:51:39 +0900

    crawling code

--- job01_crawling_headline.py
+++ job01_crawling_headline.py
@@ -8,10 +8,10 @@ category = ['Politics', 'Economic', 'Social', 'Culture', 'World', 'IT']
 url = 'https://news.naver.com/main/main.naver?mode=LSD&mid=shm&sid1=100'
 
 headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36"}
-# resp = requests.get(url, headers = headers)
+# resp = requests.get(url, headers = headers)       #
 # # print(list(resp))
 # print(type(resp))
-# soup = BeautifulSoup(resp.text, 'html.parser')
+# soup = BeautifulSoup(resp.text, 'html.parser')    # BeautifelSoup - html을 받아서 text 문서로 받음
 # # print(soup)
 # title_tags = soup.select('.sh_text_headline')
 # print(title_tags)
@@ -28,10 +28,21 @@ df_titles = pd.DataFrame()
 re_title = re.compile('[^가-힣|a-z|A-Z]')
 
 for i in range(6):
-    resp = requests.get('https://news.naver.com/main/main.naver?mode=LSD&mid=shm&sid1=10{}'.format(i),)
+    resp = requests.get('https://news.naver.com/main/main.naver?mode=LSD&mid=shm&sid1=10{}'.format(i), headers = headers)
+    soup = BeautifulSoup(resp.text, 'html.parser')
+    title_tags = soup.select('.sh_text_headline')
+    titles = []
+    for title_tag in title_tags:
+        titles.append(re.compile('[^가-힣|a-z|A-Z]').sub(' ', title_tag.text))
+    df_section_titles = pd.DataFrame(titles, columns=['titles'])
+    df_section_titles['category'] = category[i]
+    df_titles = pd.concat([df_titles, df_section_titles], axis='rows', ignore_index=True)       #  앞에꺼에 이어서 뒤에꺼를 붙여서 덮어 씌움.
 
-
-
+print(df_titles.head())
+df_titles.info()
+print(df_titles['category'].value_counts())
+df_titles.to_csv('./crawling_data/naver_headline_news_{}.csv'.format(
+    datetime.datetime.now().strftime('%Y%m%d')), index=False)
 
 
 
